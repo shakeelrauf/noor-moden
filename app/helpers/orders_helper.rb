57b4381102b8fc847695d02e288fb721c574de33
Talha Waseem <talha.waseem@phaedrasolutions.com>
2019-12-31 11:45:53 +0500

    invoice rechanges

--- app/helpers/orders_helper.rb
+++ app/helpers/orders_helper.rb
@@ -4,4 +4,16 @@ module OrdersHelper
     lineitems.collect { |item| item.total.to_f }.sum
   end
 
+  def net_total(lineitems)
+  	lineitems.collect {|qty| qty.order_qty * qty.price }.sum
+  end
+
+  def net_total_tax(lineitems)
+	(line_item_subtotal(lineitems) / 100 * 19).round(3)
+  end
+
+  def net_total_after_tax(lineitems)
+  	net_total(lineitems) + net_total_tax(lineitems)
+  end
+
 end

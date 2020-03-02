5e3f76ad20c7c551cd94f08d01380b7a6c16c9a8
Talha Waseem <talha.waseem@phaedrasolutions.com>
2020-03-02 16:36:16 +0500

    sku entry

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -145,7 +145,7 @@ class ProductsController < ApplicationController
   end
 
   def get_barcode_from_sku
-    @product = Product.where(model_number: params[:barcode].downcase).or(Product.where(model_number: params[:barcode].upcase)).first
+    @product = Product.where(model_number: params[:barcode]).first
     if @product.present?
       render json: { data: @product.as_json, status: 200 }
     else

be3c0c1ad112daefc91df95ba74bb4dd2a85a179
Talha Waseem <talha.waseem@phaedrasolutions.com>
2020-03-03 16:51:13 +0500

    highlight text after manual entry

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,7 +1,7 @@
 class Order < ApplicationRecord
 
   include PgSearch::Model
-  pg_search_scope :search_by_shopify_ids, against: [:id, :status, :created_at], using: {
+  pg_search_scope :search_by_shopify_ids, against: [:id, :status, :created_at, :label], using: {
                     tsearch: { prefix: true }
                   },associated_against: {
     lineitems: [:sku, :shopify_product_id, :variant_id]
